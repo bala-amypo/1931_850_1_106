efc073490e6758203a81c44d42a8bcbe5e92ddec
bala-amypo <bala@amypo.in>
2025-12-25 14:30:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Role.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+public class Role {
+    public static final String USER = "USER";
+    public static final String ADMIN = "ADMIN";
+    public static final String MANAGER = "MANAGER";
+
+    private Role() {
+        // Private constructor to prevent instantiation
+    }
+}
